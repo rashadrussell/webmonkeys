8881f1342411aaaa32d7754a22b73a8d32570788
Rashad <rjrusse2@illinois.edu>
2015-02-16 16:45:54 -0600

    add ajax

--- django/js/processComment.js
+++ django/js/processComment.js
@@ -1,27 +1,45 @@
+// Wait until all of the HTML and CSS is loaded on the page, before executing this JavaScript code
 $(document).ready(function() {
 
+	// Listen to when a user clicks on the 'Post' button to submit a comment
 	$('#comments .comment-form button').on('click', function(eve) {
 
-		var author  = $("#comments input").val(),
-			comment = $("#comments textarea").val(),
-			data = {author: author, comment: comment},
+		var author  = $("#comments input").val(),          // Grab the author's name from the input form
+			comment = $("#comments textarea").val(),       // Grab the comment that the author wrote
+			data = {author: author, comment: comment},     // Store author's name and the comment in a JavaScript object
 			commentItem
 
-
+		/*
+		 * Send a Post request via Ajax so that the page doesn't need to refresh,
+		 * and send that data as a JSON object to the python script. The python
+		 * script will process that data and store it in a database.
+		 */
 		$.ajax({
-			type: "POST",
-			url: "http://localhost:3000/django.py",
-			data: JSON.stringify(data),
-			dataType: "json",
-			success: function(data) {
+			type: "POST",                                  // Make Post requst
+			url: "http://localhost:3000/django.py",        // Send Post request to the django.py script
+ 			data: JSON.stringify(data),                    // Convert the data in the JavaScript object into a JSON object, before sending 
+			dataType: "json",                              // Set Post request header 'dataType' to json 
+			success: function(data) {                      // If request is successful, do something the data from the response
 			}
 		});
 
 
-		var commentItem = $('<li class="comment-item" />');
+		// Create a HTML li element, with the class set to 'comment-item'
+		var commentItem = $('<li class="comment-item" />');  
 
-		commentItem.append('<p class="author">').find('.author').append(author);
+		/*
+		 * Create a HTML p element with class 'author' and append it to the comment-item.
+		 * Find that p element, and append the author's name inside of it.
+		*/ 
+		commentItem.append('<p class="author">').find('.author').append(author); 
+
+		/*
+		 * Create a HTML p element with class 'comment' and append it to the comment-item.
+		 * Find that p element, and append the author's comment inside of it.
+		*/  
 		commentItem.append('<p class="comment">').find('.comment').append(comment);
+
+		// Prepend the entire comment item to the list of comments.
 		$('.comment-list').prepend(commentItem);
 
 	});
